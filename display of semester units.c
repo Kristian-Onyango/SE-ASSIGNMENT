f51593718cbbc8b395ce616efa966991f355913f
Kristian-Onyango <onyangokristian8@gmail.com>
2025-09-19 13:57:39 +0300

    Add files via upload
    SEMESTER 1 UNITS PRESENTATION

---
+++ display of semester units.c
@@ -0,0 +1,22 @@
+/*
+NAME:KRISTIAN ONYANGO
+REG:PA106/G/28750/25
+DESCRIPTION:CODE TO DISPLAY THE UNITS FOR THIS YEAR'S 1ST YEAR SEMESTER 1
+*/
+
+#include <stdio.h>
+
+
+int main()
+{
+  printf(" YOU HAVE  SIGNED UP FOR THE FOLLOWING UNITS:\n");
+  printf("\n1.Introduction to Programming and Problem Solving\n");
+  printf("2.Communication Skills\n");
+  printf("3.Fundamentals of ICT\n");
+  printf("4.Mathematics for Science\n");
+  printf("5.Discrete Mathematics\n");
+  printf("6.Computer Systems and Organization\n");
+  printf("7.System Analysis and Design\n");
+
+    return 0;
+}
