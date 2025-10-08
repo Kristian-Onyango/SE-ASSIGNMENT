af868b247800f5a1d4d6d5e1f8d69b81cf0a69c1
Kristian-Onyango <onyangokristian8@gmail.com>
2025-10-08 22:31:54 +0300

    NUMBER COUNTER 100 TO 50

---
+++ numbers_100_to_50.c
@@ -0,0 +1,24 @@
+/*
+NAME:KRISTIAN ONYANGO
+REG:PA106/G/28750/25
+DESCRIPTION:NUMBER COUNTER FROM 100 T0 50
+*/
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+
+
+ int i;
+
+ for (i=100; i>49; i--){
+    printf("%d\n", i);
+ }
+
+
+
+    return 0;
+}
